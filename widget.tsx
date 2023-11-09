73b62b4a45512555c369bef9162d45ae458bc31d
jsogle1 <105501317+jsogle1@users.noreply.github.com>
2023-11-09 09:53:13 -0500

    Update widget.tsx

--- widget.tsx
+++ widget.tsx
@@ -4,10 +4,9 @@ import esriLoader from 'esri-loader';
 
 const Widget = () => {
   const [clickedPoint, setClickedPoint] = useState(null);
-  const [state, setState] = useState(null);
-  const [region, setRegion] = useState(null);
-  const [county, setCounty] = useState(null);
-  const [city, setCity] = useState(null);
+  const [stateAttribute, setStateAttribute] = useState(null);
+  const [countyAttribute, setCountyAttribute] = useState(null);
+  const [cityAttribute, setCityAttribute] = useState(null);
 
   const handleMapClick = async (event) => {
     // Extract the mapPoint from the click event.
@@ -16,19 +15,16 @@ const Widget = () => {
     // Create a point variable using the mapPoint.
     setClickedPoint(mapPoint);
 
-    // Query the state, region, county, and city based on the clicked point's latitude and longitude.
-    const {
-      state: stateValue,
-      region: regionValue,
-      county: countyValue,
-      city: cityValue,
-    } = await queryAttributesFromFeatureServices(mapPoint.latitude, mapPoint.longitude);
+    // Query the state, county, and city based on the clicked point's latitude and longitude.
+    const { stateAttribute: stateValue, countyAttribute: countyValue, cityAttribute: cityValue } = await queryAttributesFromFeatureServices(
+      mapPoint.latitude,
+      mapPoint.longitude
+    );
 
-    // Update the state, region, county, and city values.
-    setState(stateValue);
-    setRegion(regionValue);
-    setCounty(countyValue);
-    setCity(cityValue);
+    // Update the state, county, and city values.
+    setStateAttribute(stateValue);
+    setCountyAttribute(countyValue);
+    setCityAttribute(cityValue);
   };
 
   // Function to query attributes from the "Municipalities" feature service based on latitude and longitude.
@@ -41,12 +37,12 @@ const Widget = () => {
       // Create a query task for the "Municipalities" feature service.
       const queryTask = new QueryTask({ url: municipalitiesServiceURL });
 
-      // Create a query to find the state, region, county, and city based on the clicked point.
+      // Create a query to find the state, county, and city based on the clicked point.
       const query = new Query();
       query.geometry = new Point({ latitude, longitude });
 
       // Specify the fields you want to retrieve for the "Municipalities" layer.
-      query.outFields = ['State', 'Region', 'COUNTY', 'City']; // Adjust the field names as needed.
+      query.outFields = ['STATE', 'COUNTY', 'NAME']; // Adjust the field names as needed.
 
       // Execute the query and retrieve the feature.
       const result = await queryTask.execute(query);
@@ -54,29 +50,26 @@ const Widget = () => {
       if (result.features.length > 0) {
         const attributes = result.features[0].attributes;
 
-        // Assuming your "Municipalities" feature service has these attribute fields.
-        const stateValue = attributes.State || 'State not found';
-        const regionValue = attributes.Region || 'Region not found';
+        // Use the specific attribute names for state, county, and city.
+        const stateValue = attributes.STATE || 'State not found';
         const countyValue = attributes.COUNTY || 'County not found';
-        const cityValue = attributes.City || 'City not found';
+        const cityValue = attributes.NAME || 'City not found';
 
-        return { state: stateValue, region: regionValue, county: countyValue, city: cityValue };
+        return { stateAttribute: stateValue, countyAttribute: countyValue, cityAttribute: cityValue };
       } else {
         // Handle the case where no feature is found.
         return {
-          state: 'State not found',
-          region: 'Region not found',
-          county: 'County not found',
-          city: 'City not found',
+          stateAttribute: 'State not found',
+          countyAttribute: 'County not found',
+          cityAttribute: 'City not found',
         };
       }
     } catch (error) {
       console.error('Error querying attributes:', error);
       return {
-        state: 'Error',
-        region: 'Error',
-        county: 'Error',
-        city: 'Error',
+        stateAttribute: 'Error',
+        countyAttribute: 'Error',
+        cityAttribute: 'Error',
       };
     }
   };
@@ -84,11 +77,6 @@ const Widget = () => {
   return (
     <div>
       <h1>Site Location</h1>
-
-      {/* Render a button to simulate a map click event */}
-      <button onClick={() => handleMapClick({ mapPoint: { latitude: 123, longitude: 456 } })}>
-        Simulate Map Click
-      </button>
 
       {/* Render the clicked point information */}
       {clickedPoint && (
@@ -105,19 +93,15 @@ const Widget = () => {
             </tr>
             <tr>
               <th>State</th>
-              <td>{state}</td>
-            </tr>
-            <tr>
-              <th>Region</th>
-              <td>{region}</td>
+              <td>{stateAttribute}</td>
             </tr>
             <tr>
               <th>County</th>
-              <td>{county}</td>
+              <td>{countyAttribute}</td>
             </tr>
             <tr>
               <th>City</th>
-              <td>{city}</td>
+              <td>{cityAttribute}</td>
             </tr>
           </tbody>
         </table>
